658f9f243a8212021f82207782faddfdff8a6f5a
ellie macbook <sergzhio238@gmail.com>
2023-03-05 21:21:46 +0300

    add sample for future view

--- app/src/main/java/com/example/analogclockview/MainActivity.kt
+++ app/src/main/java/com/example/analogclockview/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.example.analogclockview
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private val clock: AnalogClockView by lazy { findViewById(R.id.clock) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        customizeClock()
+    }
+
+    private fun customizeClock() {
+        // TODO: Fill it later
     }
 }
