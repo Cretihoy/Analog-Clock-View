eaa37310e1aa8581713a776b3b6e4dc854cedce0
ellie macbook <sergzhio238@gmail.com>
2023-03-05 21:29:08 +0300

    add colors

--- app/src/main/java/com/example/analogclockview/AnalogClockView.kt
+++ app/src/main/java/com/example/analogclockview/AnalogClockView.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.View
 import androidx.annotation.ColorRes
+import androidx.core.content.ContextCompat
 
 class AnalogClockView(
     context: Context,
@@ -17,6 +18,16 @@ class AnalogClockView(
 
     constructor(context: Context, attributeSet: AttributeSet?) : this(context, attributeSet, 0)
     constructor(context: Context) : this(context, null)
+
+    private var clockHeight = 0
+    private var clockWidth = 0
+    private var clockRadius = 0
+
+    private var watchfaceColor = getColor(R.color.defaultWatchfaceColor)
+    private var borderColor = getColor(R.color.defaultBorderColor)
+    private var handsColor = getColor(R.color.defaultHandsColor)
+    private var secondsHandColor = getColor(R.color.defaultSecondsHandsColor)
+    private var textColor = getColor(R.color.defaultTextColor)
 
     fun setWatchfaceColor(@ColorRes value: Int) {
         // TODO
@@ -40,5 +51,9 @@ class AnalogClockView(
 
     fun setTextSize(value: Float) {
         // TODO
+    }
+
+    private fun getColor(@ColorRes color: Int): Int {
+        return ContextCompat.getColor(context, color)
     }
 }
